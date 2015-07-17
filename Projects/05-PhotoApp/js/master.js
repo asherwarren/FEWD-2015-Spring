27533f8a1a500cc81eda2c3410530322e91a8298
asherwarren <asher.warren@gmail.com>
2015-07-16 20:34:06 -0400

    Photo App with Carousel

--- Projects/05-PhotoApp/js/master.js
+++ Projects/05-PhotoApp/js/master.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-	$(".photo_carousel").carousel();
+	$(".photo_carousel").carousel( {
+		show: {
+		"300px" : 2,
+		"480px" : 2,
+        "740px" : 3,
+        "960px" : 3
+    }
+	});
 });
